8703b79f29008c35d7ef35503964feda3dc0f346
analuuuiza <anaedara123@gmail.com>
2023-09-21 15:17:16 -0300

    listagem e usuários

---
+++ src/router/AppRouter.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import BuscaCep from "../components/BuscaCepForm";
+import Cadastro from "../components/Cadastro";
+import Listagem from "../components/Listagem";
+
+const AppRouter = () => {
+    return (
+       <BrowserRouter>
+         <Routes>
+           <Route path="buscacep" element={<BuscaCep />}></Route>
+           <Route path="cadastro" element={<Cadastro />}></Route>
+           <Route path="listagem" element={<Listagem  />}></Route>
+
+        </Routes>
+       </BrowserRouter>
+    );
+}
+ export default AppRouter;
