fa4aec08d4ee0b6569de25a54f9d3f6bb550b861
analuuuiza <anaedara123@gmail.com>
2023-11-07 10:10:57 -0300

    atualizar e editar

--- src/router/AppRouter.tsx
+++ src/router/AppRouter.tsx
@@ -4,6 +4,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import BuscaCep from "../components/BuscaCepForm";
 import Cadastro from "../components/Cadastro";
 import Listagem from "../components/Listagem";
+import Editar from "../components/Editar";
 
 const AppRouter = () => {
     return (
@@ -12,6 +13,7 @@ const AppRouter = () => {
            <Route path="buscacep" element={<BuscaCep />}></Route>
            <Route path="cadastro" element={<Cadastro />}></Route>
            <Route path="listagem" element={<Listagem  />}></Route>
+           <Route path="/editar/:id"element={<Editar />}></Route>
 
         </Routes>
        </BrowserRouter>
